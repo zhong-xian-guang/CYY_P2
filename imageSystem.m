b38e1dd5a2fdf60c070c5162bc7cfdcf12a41e02
DESKTOP-VMUV0NS\Graphics <ea5878158@gmail.com>
2018-05-14 22:45:10 +0800

    還沒好的東東

--- imageSystem.m
+++ imageSystem.m
@@ -6,6 +6,32 @@ classdef imageSystem
             image = imread(path);
             img = rgb2gray(image);
         end
+        function ret = detectFeature(image,windowSize)
+            %1.color to grayscal
+            I = image;
+            imshow(I);
+            %2.Spatial derivative calculation
+            [Ix, Iy] = gradient(double(I)); % first order partials
+            [Ixx, Ixy] = gradient(Ix);      % second order partials
+            [Iyx, Iyy] = gradient(Iy);      % second order partials
+            M = 
+            figure();
+            imshow(Ix);
+            figure();
+            imshow(Iy);
+            figure();
+            imshow(Ixx);
+            figure();
+            imshow(Ixy);
+            figure();
+            imshow(Iyx);
+            figure();
+            imshow(Iyy);
+            %3.Structure tensor setup
+            %4.Harris response calculation
+            %5.Non-maximum suppression
+            
+        end
     end
 end
 
