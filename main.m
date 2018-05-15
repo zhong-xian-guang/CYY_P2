39bad8715a6f6bb075b33079e4e2efe353c82a87
DESKTOP-VMUV0NS\Graphics <ea5878158@gmail.com>
2018-05-15 15:03:32 +0800

    完成 haha detection

--- main.m
+++ main.m
@@ -7,8 +7,15 @@ clc;
 %image matching
 %bundle adjustment and blending
 img = imageSystem.readGrayImage('data/grail/grail00.jpg');
+%img = imageSystem.readGrayImage('gtest.png');
+c = imageSystem.detectFeature(img,8);
 C = corner(img);
-
+figure;
 imshow(img);
 hold on
 plot(C(:,1),C(:,2),'r*');
+
+figure;
+imshow(img);
+hold on
+plot(c(:,1),c(:,2),'r*');
